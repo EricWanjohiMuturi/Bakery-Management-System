554139c94acd5365c07c385772ca15ff7a1d9129
EricWanjohiMuturi <emuturi124@gmail.com>
2025-08-31 09:24:30 +0300

    add products to the offcanvas cart

--- static/js/store.js
+++ static/js/store.js
@@ -1,38 +1,58 @@
 document.addEventListener("alpine:init", () => {
-// Products store
-Alpine.store("products", {
+  // Products store
+  Alpine.store("products", {
     all: [],
     async fetch() {
-        let res = await fetch("/api/products/");
-        this.all = await res.json();
-    }
-});
+      let res = await fetch("/api/products/");
+      this.all = await res.json();
+    },
+  });
 
-// Cart store
-Alpine.store("cart", {
-    items: [],
+  // Cart store
+  Alpine.store("cart", {
+    items: JSON.parse(localStorage.getItem("cart")) || [],
+
+    save() {
+      localStorage.setItem("cart", JSON.stringify(this.items));
+    },
+
+    get count() {
+      return this.items.reduce((acc, i) => acc + i.quantity, 0);
+    },
+
+    get total() {
+      return this.items.reduce((acc, i) => acc + i.quantity * parseFloat(i.price), 0).toFixed(2);
+    },
+
     add(product) {
-    let existing = this.items.find(i => i.id === product.id);
-    if (existing) {
+      let existing = this.items.find((i) => i.id === product.id);
+      if (existing) {
         existing.quantity++;
-    } else {
+      } else {
         this.items.push({ ...product, quantity: 1 });
-    }
+      }
+      this.save();
     },
+
     remove(productId) {
-    this.items = this.items.filter(i => i.id !== productId);
+      this.items = this.items.filter((i) => i.id !== productId);
+      this.save();
     },
+
     increment(productId) {
-    let item = this.items.find(i => i.id === productId);
-    if (item) item.quantity++;
+      let item = this.items.find((i) => i.id === productId);
+      if (item) item.quantity++;
+      this.save();
     },
+
     decrement(productId) {
-    let item = this.items.find(i => i.id === productId);
-    if (item && item.quantity > 1) {
+      let item = this.items.find((i) => i.id === productId);
+      if (item && item.quantity > 1) {
         item.quantity--;
-    } else {
+      } else {
         this.remove(productId);
-    }
-    }
-});
+      }
+      this.save();
+    },
+  });
 });
